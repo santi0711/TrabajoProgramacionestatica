5e7d00ed53654f494fb13b8bcc80d2ca681a327b
Santiago <santiagohue07@gmail.com>
2025-10-29 19:03:31 -0300

    Avanzo con el boceto

--- JS/boceto.js
+++ JS/boceto.js
@@ -1,1 +1,8 @@
+function entrar() {
+  // Efecto de desvanecimiento
+  document.body.style.opacity = "0";
+  setTimeout(() => {
+    window.location.href = "boceto.html"; // Redirige al boceto
+  }, 1000);
+}
 
